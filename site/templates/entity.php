751a96578aaff41c174b99d4727922e0d83098d7
moritzebeling <git@moritzebeling.com>
2019-11-06 11:39:23 +0100

    reduce templates to minimum

--- site/templates/entity.php
+++ site/templates/entity.php
@@ -2,16 +2,6 @@
 
 snippet('header');
 
-// snippet('navigation/history');
-
-?>
-
-<div id="framework" class="grid panels"></div>
-
-<?php
-
-// snippet('navigation/archive');
-
 snippet('footer', [
     'include' => [
         js('media/plugins/centre-for-documentary-architecture/framework/public/bundle.js', ['defer'=>true]),
