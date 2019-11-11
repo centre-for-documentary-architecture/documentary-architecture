9869fd6461ba201658e8600002e8bb10cbc33cb4
moritzebeling <git@moritzebeling.com>
2019-11-11 16:55:55 +0100

    let archive bar link to archive

--- site/plugins/framework/assets/src/components/navigation/archive.svelte
+++ site/plugins/framework/assets/src/components/navigation/archive.svelte
@@ -2,10 +2,17 @@
 
     export let entity;
 
+    // const location = new URL( url );
+    let url = location.origin + '/archive';
+
 </script>
 
 <nav class="col-12 bar archive horizontal">
     <ul>
-        <li><a href="#top">Archive</a></li>
+        <li>
+            <a on:click={window.navi} title="Go to Archive" href="{url}" data-template="archive">
+                Archive
+            </a>
+        </li>
     </ul>
 </nav>
