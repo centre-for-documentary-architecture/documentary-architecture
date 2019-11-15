d31cbec2974501780599ea84d01c52fffc8ffaac
moritzebeling <git@moritzebeling.com>
2019-11-15 19:34:52 +0100

    finally remount video player

--- site/plugins/framework/assets/src/components/views/video.svelte
+++ site/plugins/framework/assets/src/components/views/video.svelte
@@ -1,8 +1,5 @@
 <script>
 
-	import { onDestroy } from 'svelte';
-	import { beforeUpdate } from 'svelte';
-
 	export let view;
 
 	import Transcript from './transcript.svelte';
@@ -10,24 +7,18 @@
 
 	export let classname = 'preview';
 
-	beforeUpdate(() => {
-		console.log('beforeUpdate');
-		if( mediaElement === false ){
-			return false;
-		}
-		mediaElement.pause();
-	});
-	onDestroy(() => {
-		console.log('beforeUpdate');
-		if( mediaElement === false ){
-			return false;
-		}
-		mediaElement.remove();
-	});
-
-	let mediaElement = false;
+	let mediaElement;
 
 	let videoWidth = 0;
+
+	let video = true;
+	$: videoSource = view.content.srcset[0].url;
+	$: { reMountVideo( videoSource ) }
+	function reMountVideo( xx ){
+		console.log('reMountVideo',xx);
+		video = false;
+		setTimeout(() => video = true, 0);
+	}
 
 	function preventContextMenu( e ){
 		e.preventDefault();
@@ -45,15 +36,20 @@
 	</h3>-->
 
 	<div class="section--content">
-		<video width="100%" height="auto" on:contextmenu={preventContextMenu} controls poster="{ view.content.poster }" bind:this={mediaElement} preload="metadata" >
+		{#if video === true}
+			<video width="100%" height="auto" controls preload="metadata"
+				on:contextmenu={preventContextMenu} poster="{ view.content.poster }" bind:this={mediaElement}>
 
-			{#each view.content.srcset as source}
-				{#if videoWidth < source.width }
-					<source type="{source.mime}" media="{source.media}" src="{source.url}" >
-				{/if}
-			{/each}
+				{#each view.content.srcset as source}
+					{#if videoWidth < source.width }
+						<source type="{source.mime}" media="{source.media}" src="{source.url}" >
+					{/if}
+				{/each}
 
-		</video>
+			</video>
+		{:else}
+			Remount
+		{/if}
 	</div>
 
 	{#if transcript}
