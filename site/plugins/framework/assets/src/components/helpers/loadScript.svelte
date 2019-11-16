56207d45d37e176d14f69ce2f3579c0d3859e842
moritzebeling <git@moritzebeling.com>
2019-11-16 14:58:00 +0100

    create loadScript helper component

---
+++ site/plugins/framework/assets/src/components/helpers/loadScript.svelte
@@ -0,0 +1,23 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    let script = document.createElement('script');
+
+    export let src;
+	script.src = src;
+	script.onload = function() {
+        dispatch('loaded', {
+            loaded: true
+        });
+        console.log( 'Load script '+src );
+	};
+    document.body.appendChild(script);
+
+    import { onDestroy } from 'svelte';
+
+    onDestroy(() => {
+        script.remove();
+        console.log( 'Remove script '+src );
+    });
+</script>
