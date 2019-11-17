00752cf249f175807ac905842b0b437633fe4dd0
moritzebeling <git@moritzebeling.com>
2019-11-17 12:05:04 +0100

    improve interstatial

--- site/plugins/frontend/assets/src/components/helpers/wait.svelte
+++ site/plugins/frontend/assets/src/components/helpers/wait.svelte
@@ -2,7 +2,7 @@
     .loader {
 		width: 100%;
 		height: 100%;
-		background-color: #000;
+		background-color: rgba(0,0,0,0.5);
 		/* z-index: 30; */
 		position: absolute;
 		top: 0;
@@ -21,21 +21,21 @@
 		border-radius: 5rem;
 		z-index: 1;
 		background-color: #fff;
-		-webkit-animation: pulsate 2s ease infinite both;
-				animation: pulsate 2s ease infinite both;
+		-webkit-animation: pulsate 1.5s ease infinite both;
+				animation: pulsate 1.5s ease infinite both;
 	}
 	@keyframes pulsate {
 		0% {
-		-webkit-transform: scale(0.8);
-				transform: scale(0.8);
+		-webkit-transform: scale(0.5);
+				transform: scale(0.5);
 		}
 		50% {
-		-webkit-transform: scale(1.6);
-				transform: scale(1.6);
+		-webkit-transform: scale(1);
+				transform: scale(1);
 		}
 		100% {
-		-webkit-transform: scale(0.8);
-				transform: scale(0.8);
+		-webkit-transform: scale(0.5);
+				transform: scale(0.5);
 		}
     }
 </style>
