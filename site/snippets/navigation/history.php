08575dbd4d985dbd8994349918c7631d9e8067fd
moritzebeling <git@moritzebeling.com>
2020-01-15 18:04:11 +0100

    adjust history bar for home or other templates

--- site/snippets/navigation/history.php
+++ site/snippets/navigation/history.php
@@ -1,7 +1,13 @@
 <nav class="bar history horizontal white">
   <h3>
-    <a href="#top" class="smooth">
-      <?= html( $site->title() ) ?>
-    </a>
+    <?php if( $page->isHomePage() ): ?>
+      <a href="#top" class="smooth">
+        <?= html( $site->title() ) ?>
+      </a>
+    <?php else: ?>
+      <a href="<?= $site->homePage()->url() ?>">
+        <?= html( $site->title() ) ?>
+      </a>
+    <?php endif; ?>
   </h3>
 </nav>
