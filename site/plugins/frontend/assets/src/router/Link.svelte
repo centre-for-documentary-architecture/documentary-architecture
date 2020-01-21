0a21a5a83ed3cea95731d50d78939e2bcdb20cd0
moritzebeling <git@moritzebeling.com>
2020-01-21 02:45:24 +0100

    add link component and anyLink function

---
+++ site/plugins/frontend/assets/src/router/Link.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { navigateTo } from './navigate.js';
+
+	import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  export let href = '/';
+	export let title = '';
+  export let classList = '';
+  export { classList as class };
+
+  function onClick(event) {
+    navigateTo(href);
+    dispatch('click', event);
+  }
+</script>
+
+<a {href} class={classList} {title} on:click|preventDefault={onClick}>
+	<slot>{title}</slot>
+</a>
