89739d76b107f36f0ab9908a7784fb5dbaf43732
moritzebeling <git@moritzebeling.com>
2020-01-25 12:24:34 +0100

    work on about cda bar

--- site/snippets/navigation/cda.php
+++ site/snippets/navigation/cda.php
@@ -1,17 +1,17 @@
-<div id="cda" class="bar cda vertical" onclick="openAboutBar(event)">
+<aside id="cda" class="bar cda vertical" onclick="openAboutBar(event)" title="More info on <?= $site->title() ?>">
 
-    <a class="logo abbr">
-        <?= svg('assets/img/cda-logo-bar.svg'); ?>
-    </a>
-    <a class="logo full" onclick="closeAboutBar(event)">
-        <?= svg('assets/img/cda-logo-full.svg'); ?>
-    </a>
+  <div class="panel">
 
-    <aside class="about">
+    <a class="logo abbr"><?= svg('assets/img/cda-logo-bar.svg'); ?></a>
+    <a class="logo full" onclick="closeAboutBar(event)"><?= svg('assets/img/cda-logo-full.svg'); ?></a>
+    <button class="bar-close" onclick="closeAboutBar(event)" title="Close">&times;</button>
+
+    <div class="about">
         <?php snippet('about/about'); ?>
-    </aside>
+    </div>
 
-    <button class="bar-close" onclick="closeAboutBar(event)">&times;</button>
+  </div>
 
-</div>
-<div class="glass" onclick="closeAboutBar(event)"></div>
+  <div class="glass" onclick="closeAboutBar(event)"></div>
+
+</aside>
