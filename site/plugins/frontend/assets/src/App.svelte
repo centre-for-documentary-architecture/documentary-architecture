ccf4638d1010e4ecb847c3c735e56e28482e290c
moritzebeling <git@moritzebeling.com>
2020-01-27 00:27:15 +0100

    remove comment

--- site/plugins/frontend/assets/src/App.svelte
+++ site/plugins/frontend/assets/src/App.svelte
@@ -40,11 +40,11 @@
 	Worlditem: {page.worlditem || '?'}
 {/if}
 
-{#if page.template === 'entity' }
+{#if page.html }
+	{@html page.html}
+{:else if page.template === 'entity' }
 	<EntityTemplate {page} />
 	<ArchiveBar {page} />
 {:else if page.template === 'archive' }
 	<ArchiveTemplate {page} />
-{:else if page.html }
-	{@html page.html}
 {/if}
